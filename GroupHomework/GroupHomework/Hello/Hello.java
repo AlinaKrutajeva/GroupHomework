27cb35ff93e9adc402e61f1bf871ec9536031dce
AlinaKrutajeva <a_k_a@inbox.lv>
2022-03-14 18:21:39 +0200

    Hello!

--- GroupHomework/GroupHomework/Hello/Hello.java
+++ GroupHomework/GroupHomework/Hello/Hello.java
@@ -1,3 +1,5 @@
+package GroupHomework.GroupHomework.Hello;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello!");
--- GroupHomework/Javaguru_QA2_Lectures/Lecture 2/Tasks/Exercise1.java
+++ GroupHomework/Javaguru_QA2_Lectures/Lecture 2/Tasks/Exercise1.java
@@ -6,7 +6,7 @@ public class Exercise1 {
 
     public static void main(String[] args) {
 //        String name = "Konstantins";
-//        System.out.println("Hello, my name is " + name);
+//        System.out.println("GroupHomework.GroupHomework.Hello.Hello, my name is " + name);
 
 //        int a = 10;
 //        int b = 4;
